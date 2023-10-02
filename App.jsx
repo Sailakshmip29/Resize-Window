57c89b2dbff9bd75d00d41098695c521948eceac
Sailakshmip29 <134680796+Sailakshmip29@users.noreply.github.com>
2023-10-02 16:17:29 +0530

    create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Resize from './screensize/Resize.jsx';
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Resize />
+    </div>
+  );
+}
+
+export default App;
